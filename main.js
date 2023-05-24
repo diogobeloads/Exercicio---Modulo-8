c9a50938b8e184cec44a7613e1c71676048a0c96
Diogo Belo <diogobelodss22@gmail.com>
2023-05-23 23:47:39 -0300

    exercicio de js, agenda de contatos

---
+++ main.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form-contato');
+const imgAdicionado = '<img src="./images/adicionado.png" alt="emoji ok" />';
+const spanAdicionado = '<span class="contato adicionado">Adicionado</span>';
+const nomeContato = [];
+const numeroContato = [];
+let linhas = " ";
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nomeContato');
+    const inputNumeroContato = document.getElementById('numeroContato');
+
+    if (nomeContato.includes(inputNomeContato.value)) {
+        alert (`O contato: ${inputNomeContato.value} já foi inserido`);
+    } else {
+        nomeContato.push(inputNomeContato.value);
+        numeroContato.push(parseInt(inputNumeroContato.value));
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += `<td>${imgAdicionado}`;
+    linha += `</tr>`;
+
+    linhas += linha;
+    }    
+}
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
